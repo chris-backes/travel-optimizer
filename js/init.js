3c331d7ab9ddf28361afa971f8e9a95e55a45a94
Christopher Backes <you@example.com>
2022-01-05 19:51:05 -0500

    attemptig to refactor lcoal storage/refactored date

--- js/init.js
+++ js/init.js
@@ -4,15 +4,15 @@ var cityInputEl = document.querySelector("#cityname");
 var cityContainerEl = document.querySelector("#city-container");
 var citySearchTerm = document.querySelector("#city-search-term");
 
-var getCityHistory = JSON.parse(localStorage.getItem("cityArr")) || [];
-//console.log(typeof getCityHistory);
-for (i = 0; i < getCityHistory.length; i++) {
-  var cityBtn = document.createElement("button");
-  cityBtn.innerHTML = getCityHistory[i];
-  cityBtn.setAttribute("data-city", getCityHistory[i]);
-  cityBtn.classList = "btn";
-  cityButtonsEl.appendChild(cityBtn);
-}
+// var getCityHistory = JSON.parse(localStorage.getItem("cityArr")) || [];
+// //console.log(typeof getCityHistory);
+// for (i = 0; i < getCityHistory.length; i++) {
+//   var cityBtn = document.createElement("button");
+//   cityBtn.innerHTML = getCityHistory[i];
+//   cityBtn.setAttribute("data-city", getCityHistory[i]);
+//   cityBtn.classList = "btn";
+//   cityButtonsEl.appendChild(cityBtn);
+// }
 
 var formSubmitHandler = function (event) {
   // prevent page from refreshing
@@ -23,16 +23,13 @@ var formSubmitHandler = function (event) {
 
   if (cityname) {
     getCityData(cityname);
-    var cityBtn = document.createElement("button");
-    cityBtn.innerHTML = cityname;
-    cityBtn.setAttribute("data-city", cityname);
-    cityBtn.classList = "btn";
-    cityButtonsEl.appendChild(cityBtn);
+    // var cityBtn = document.createElement("button");
+    // cityBtn.innerHTML = cityname;
+    // cityBtn.setAttribute("data-city", cityname);
+    // cityBtn.classList = "btn";
+    // cityButtonsEl.appendChild(cityBtn);
 
-    getCityHistory.push(cityname);
-
-    localStorage.setItem("cityArr", JSON.stringify(getCityHistory));
-
+    localStoring(cityName);
     // clear old content
     //cityContainerEl.textContent = "";
     cityInputEl.value = "";
@@ -103,14 +100,14 @@ var getCity = function (city) {
 
 var displayCity = function (citydata, searchTerm) {
   var cityContainer = document.querySelector("#city-container");
-  cityContainer.innerHTML = '';
+  cityContainer.innerHTML = "";
   // check if api returned any data
   if (!citydata) {
     cityContainerEl.textContent = "No city found.";
     return;
   }
-  var date = moment().format("MM-DD-YYYY"); //how do I add the current date to the screen?
-  citySearchTerm.textContent = searchTerm + "  " + date;
+  citySearchTerm.textContent =
+    searchTerm + "  " + new Date().toLocaleDateString();
 
   // create a link for each historical site
   var cityLstEl = document.createElement("ul");
@@ -119,77 +116,129 @@ var displayCity = function (citydata, searchTerm) {
   // citydata.features.sort((a, b) => parseInt(b.properties.rate) - parseInt(a.properties.rate));
   // console.log(citydata.features);
 
-
   for (i = 0; i < citydata.features.length; i++) {
-    var historic_places = citydata.features[i].properties.name; 
+    var historic_places = citydata.features[i].properties.name;
     var cityLst = document.createElement("li");
     //cityLst.innerHTML = getCityHistory[i];
     //cityLst.setAttribute("data-city", historic_places);
     //cityLst.classList = "li";
     cityLst.textContent = historic_places;
     cityLstEl.appendChild(cityLst);
-  
   }
- 
- cityContainer.appendChild(cityLstEl);
 
+  cityContainer.appendChild(cityLstEl);
 
   //var uv_Index = citydata.uvi; //still searching to find the UV index
 
-
   //weatherEl.classList = "list-item flex-row justify-space-between align-center";
 
-//   var cityNameEl = document.querySelector("#city-container h2");
-//   cityNameEl.textContent = searchTerm;
-//   var temperatureEl = document.querySelector(
-//     "#city-container .temperature span"
-//   );
-//   temperatureEl.textContent = Temp;
+  //   var cityNameEl = document.querySelector("#city-container h2");
+  //   cityNameEl.textContent = searchTerm;
+  //   var temperatureEl = document.querySelector(
+  //     "#city-container .temperature span"
+  //   );
+  //   temperatureEl.textContent = Temp;
 
-//   var windEl = document.querySelector("#city-container .wind span");
-//   windEl.textContent = wind_speed;
+  //   var windEl = document.querySelector("#city-container .wind span");
+  //   windEl.textContent = wind_speed;
 
-//   var humidityEl = document.querySelector("#city-container .humidity span");
-//   humidityEl.textContent = humidity;
+  //   var humidityEl = document.querySelector("#city-container .humidity span");
+  //   humidityEl.textContent = humidity;
 
-//   var uviEl = document.querySelector("#city-container .uvi span");
-//   uviEl.textContent = uvi;
-//   //WORK ON THE UVI INDICATOR
-//   var uviBox = document.querySelector("#uviBox");
+  //   var uviEl = document.querySelector("#city-container .uvi span");
+  //   uviEl.textContent = uvi;
+  //   //WORK ON THE UVI INDICATOR
+  //   var uviBox = document.querySelector("#uviBox");
 
-//   if (parseInt(uvi) < 3) {
-//     uviBox.classList = "good";
-//     //cityButtonsEl.appendChild(uviBox);
-//   }
-//   if (parseInt(uvi) == 3) {
-//     uviBox.classList = "okay";
-//   }
+  //   if (parseInt(uvi) < 3) {
+  //     uviBox.classList = "good";
+  //     //cityButtonsEl.appendChild(uviBox);
+  //   }
+  //   if (parseInt(uvi) == 3) {
+  //     uviBox.classList = "okay";
+  //   }
 
-//   if (parseInt(uvi) > 3) {
-//     uviBox.classList = "bad";
-//   }
+  //   if (parseInt(uvi) > 3) {
+  //     uviBox.classList = "bad";
+  //   }
 
-//   // append container to the dom
-//   cityContainerEl.appendChild(weatherEl);
-//   /* // create a status element
-//     var statusEl = document.createElement("span");
-//     statusEl.classList = "flex-row align-center";
+  //   // append container to the dom
+  //   cityContainerEl.appendChild(weatherEl);
+  //   /* // create a status element
+  //     var statusEl = document.createElement("span");
+  //     statusEl.classList = "flex-row align-center";
 
-//     // check if current repo has issues or not
-//     if (repos[i].open_issues_count > 0) {
-//       statusEl.innerHTML =
-//         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-//     } else {
-//       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-//     } */
+  //     // check if current repo has issues or not
+  //     if (repos[i].open_issues_count > 0) {
+  //       statusEl.innerHTML =
+  //         "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+  //     } else {
+  //       statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+  //     } */
 
-//   //MB attempt at displaying 5 day forecast
+  //   //MB attempt at displaying 5 day forecast
 
-//   // create variables for temperature
-
-  
+  //   // create variables for temperature
 };
 
 // add event listeners to form and button container
+
+// Local Storage - Chris Backes
+function localStorage(city) {
+  let previousSearch = JSON.parse(localStorage.getItem("search-history"));
+  let searchHistory = [];
+  //loads search history, if any
+  if (previousSearch) {
+    for (let i = 0; i < previousSearch.length; i++) {
+      searchHistory.push(previousSearch[i]);
+    }
+  }
+  //capitalizes each word in the search if they are not already
+  let newTerm = city.trim();
+  if (newTerm.includes(" ")) {
+    let newTermArray = newTerm.split(" ");
+    for (let i = 0; i < newTermArray.length; i++) {
+      newTermArray[i] =
+        newTermArray[i][0].toUpperCase() + newTermArray[i].substr(1);
+    }
+    newTerm = newTermArray.join(" ");
+  } else {
+    newTerm = newTerm[0].toUpperCase() + newTerm.substr(1);
+  }
+  //checks to see if the search term is already in the array, then adds it to array if not in there
+  if (!searchHistory.includes(newTerm)) {
+    searchHistory.unshift(newTerm);
+  }
+  //search history maxes out at 8 terms
+  if (searchHistory.length > 8) {
+    searchHistory.pop();
+  }
+  //stores search history
+  localStorage.setItem("search-history", JSON.stringify(searchHistory));
+}
+
+// Chris Backes -- grabs sotrage and places it in the webpage
+function grabStorage() {
+  console.log("hello");
+  //pulls info from local storage. that info is then displayed below the search bar
+  let searchHistory = JSON.parse(localStorage.getItem("search-history"));
+  if (searchHistory) {
+    for (let i = 0; i < searchHistory.length; i++) {
+      $("#city-buttons").append(
+        "<button class='btn' data-city=" +
+          searchHistory[i] +
+          ">" +
+          searchHistory[i] +
+          "</button>"
+      );
+    }
+    //event listener is added to each button that initiates get weather
+    $(".btn-secondary").on("click", function () {
+      getWeather($(this).text());
+    });
+  }
+}
+
 cityFormEl.addEventListener("submit", formSubmitHandler);
 cityButtonsEl.addEventListener("click", buttonClickHandler);
+$(document).ready(grabStorage);
